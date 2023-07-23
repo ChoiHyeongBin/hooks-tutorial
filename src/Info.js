91d083a7b31321fa42cdfb988158576ca51bc4bd
ChoiHyeongBin <chekqhksha@gmail.com>
2023-07-23 16:56:28 +0900

    hooks 예제 - 최초 커밋

---
+++ src/Info.js
@@ -0,0 +1,64 @@
+import { useReducer } from "react";
+
+function reducer(state, action) {
+  console.log("reducer");
+  console.log("state: ", state);
+  console.log("action: ", action);
+  return {
+    ...state,
+    [action.name]: action.value,
+  };
+}
+
+const Info = () => {
+  const [state, dispatch] = useReducer(reducer, {
+    name: "",
+    nickname: "",
+  });
+  const { name, nickname } = state;
+  const onChange = (e) => {
+    console.log("e.target: ", e.target);
+    dispatch(e.target);
+  };
+
+  return (
+    <div>
+      <div>
+        <input name="name" value={name} onChange={onChange} />
+        <input name="nickname" value={nickname} onChange={onChange} />
+      </div>
+      <div>
+        <div>
+          <b>이름:</b> {name}
+        </div>
+        <div>
+          <b>닉네임:</b> {nickname}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Info;
+
+// useEffect
+/* const [name, setName] = useState("");
+  const [nickname, setNickname] = useState("");
+  useEffect(() => {
+    console.log("effect");
+    console.log(name);
+
+    // 뒷정리 함수
+    return () => {
+      console.log("cleanup");
+      console.log(name);
+    };
+  }, []);
+
+  const onChangeName = (e) => {
+    setName(e.target.value);
+  };
+
+  const onChangeNickname = (e) => {
+    setNickname(e.target.value);
+  }; */
